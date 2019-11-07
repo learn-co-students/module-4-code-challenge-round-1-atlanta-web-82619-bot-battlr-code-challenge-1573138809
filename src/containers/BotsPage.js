eb0be25d1ce3e834456ff4875eef70f91fb687ee
Nick Lunn <nrlunn325@gmail.com>
2019-11-07 10:34:41 -0500

    bots can enlist and leave army on click

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,52 @@
 import React from "react";
+import YourBotArmy from './YourBotArmy'
+import BotCollection from './BotCollection'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+  constructor(){
+    super();
+    this.state = {
+      bots: [],
+      botArmy: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(response => response.json())
+    .then(botsData => {
+      this.setState({
+        bots: botsData
+      })
+    })
+  }
+
+  enlistBot = (bot) => {
+    let newBotArmy;
+    if (!this.state.botArmy.includes(bot)){
+      newBotArmy = [...this.state.botArmy, bot]
+      this.setState({
+        botArmy: newBotArmy
+      })
+    } else {
+      alert('This bot is already enlisted!')
+    }
+  }
+
+  goAWOL = (bot) => {
+    let newBotArmy = this.state.botArmy.filter(robot => robot !== bot)
+    this.setState({
+      botArmy: newBotArmy
+    })
+    
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy goAWOL={this.goAWOL} botArmy={this.state.botArmy}/>
+        <BotCollection enlistBot={this.enlistBot} bots={this.state.bots}/>
       </div>
     );
   }
